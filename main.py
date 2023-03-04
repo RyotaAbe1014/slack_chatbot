d9e65a6f828fc88fb50781e4e8d96fe8f02e1688
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-03-04 21:13:04 +0900

    add: DB保存機能作成

--- main.py
+++ main.py
@@ -4,9 +4,32 @@ from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+# sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
 load_dotenv(verbose=True)
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
+
+
+#  DB接続
+engine = create_engine('sqlite:///slack.db', echo=True)
+Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+session = Session()
+Base = declarative_base()
+
+
+# テーブル定義
+class Message(Base):
+    __tablename__ = 'messages'
+    id = Column(Integer, primary_key=True)
+    text = Column(String)
+
+
+Base.metadata.create_all(bind=engine)
 
 SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 SLACK_APP_TOKEN = os.environ.get("SLACK_APP_TOKEN")
@@ -21,9 +44,17 @@ def respond_to_mention(event, say):
     mention = event['text'].split(' ')[0]
     # メンションが@testbotだった場合のみ、メッセージを保存する
     if mention == '<@U04S5VCS3JP>':
-        say("保存しました")
-    else:
-        say("not mention @testbot")
+        try:
+            message = event['text'].split(' ')[1]
+            # メッセージを保存する
+            session.add(Message(text=message))
+            session.commit()
+            session.close()
+            say("保存しました")
+        except:
+            session.rollback()
+            session.close()
+            say("保存に失敗しました")
 
 
 SocketModeHandler(app, SLACK_APP_TOKEN).start()
